60e3e673f2d84bfe33566ca7d6e6f3d580d5c7ed
ArRolin <gmarrolin@gmail.com>
2022-08-30 15:52:41 +0600

    03 Values and Variables added

---
+++ 03_values_and_variables/script.js
@@ -0,0 +1,10 @@
+console.log("Ashiq");
+console.log(30);
+
+let firstName = "Ashiq";
+console.log(firstName);
+
+let PI = 3.1415;
+
+let myFirstJob = "Designer";
+let myCurrentJob = "Software Developer";
