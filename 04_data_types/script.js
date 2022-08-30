a7c5c79522d6c2f674b22cdc5b8ad39cc8c1c437
ArRolin <gmarrolin@gmail.com>
2022-08-30 16:26:58 +0600

    04 Data Types Added

---
+++ 04_data_types/script.js
@@ -0,0 +1,24 @@
+// Single Line Comment
+
+/* 
+Multi
+Line
+Comment
+*/
+
+// Both Type of comments are ignore by Javascript
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof 30);
+console.log(typeof "Ashiq");
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
