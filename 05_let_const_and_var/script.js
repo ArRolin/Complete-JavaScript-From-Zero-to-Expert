c672ebfdffc20d337609e620a19a20edd592da2c
ArRolin <gmarrolin@gmail.com>
2022-08-30 17:01:49 +0600

    05 Let Const and Var Added

---
+++ 05_let_const_and_var/script.js
@@ -0,0 +1,17 @@
+
+// let value is mutable or re-assignable
+
+let age = 30;
+age = 31;
+
+// let is Block Scoped
+
+// const value cant mutable or re-assignable
+
+const birthYear = 1990;
+
+
+var job = "Designer";
+job = "Software Developer";
+
+// Var is Functions Scoped
