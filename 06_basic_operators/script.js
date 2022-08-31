75ebb8e109f141ee065ca49e0929fdede31afc64
ArRolin <gmarrolin@gmail.com>
2022-08-31 12:54:40 +0600

    06 Basic Operators Added

---
+++ 06_basic_operators/script.js
@@ -0,0 +1,27 @@
+ // Assignment Operator
+
+const now = 2022;
+
+const ageAshiq = now - 1990 ;
+
+console.log(ageAshiq);
+
+let firstName = "Ashiqur";
+let lastName = "Rahaman";
+
+console.log(firstName + " " + lastName);
+
+// Math Operator
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 5; 
+x++;
+x--;
+
+console.log(x);
+
+// Comparison operator
+
+console.log(10 > 5); // >, <, >=, <=
+console.log(10 >= 10);
