4c01d6137b88c71348d78c1d85b6a1f52e05548f
ArRolin <gmarrolin@gmail.com>
2022-09-01 08:23:49 +0600

    11 Coding Challenge 02 Added

---
+++ 11_coding_challenge_02/script.js
@@ -0,0 +1,23 @@
+// Coding Challenge 02
+
+const markMass = 78;
+const markHeight = 1.69;
+
+const johnMass = 92;
+const johnHeight = 1.95;
+
+const markBMI = markMass / markHeight ** 2;
+
+const johnBMI = johnMass / johnHeight ** 2;
+
+// const markHigherBMI = markBMI > johnBMI;
+
+// console.log(markBMI, johnBMI, markHigherBMI);
+
+if(markBMI > johnBMI) {
+    console.log(`"Mark's BMI is higher than John's!`);
+} else {
+    console.log(`John's BMI is higher than Mark's!`);
+}
+
+// console.log(``);
