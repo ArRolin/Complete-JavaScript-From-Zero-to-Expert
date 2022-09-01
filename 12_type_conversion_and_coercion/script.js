e18fbbe26fb8fb5e670a3937c21b4fe7b6cb5e95
ArRolin <gmarrolin@gmail.com>
2022-09-01 13:20:17 +0600

    12 Type Conversion and Coercion

---
+++ 12_type_conversion_and_coercion/script.js
@@ -0,0 +1,16 @@
+// Type Conversion
+const inputYear = "1990";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("Ashiq"));
+
+console.log(typeof NaN);
+
+console.log(String(31), typeof String(31));
+
+// Type coercion
+
+console.log("I am " + 31 + " Years Old!");
+console.log(10+"5"-5);
+console.log(10+5-"5");
