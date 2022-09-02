cc903cd76f781a89ea6ec73c96c2dda169a19f99
ArRolin <gmarrolin@gmail.com>
2022-09-02 11:26:05 +0600

    18 Statements and Expressions Topics Added

---
+++ 18_statements_and_expressions/script.js
@@ -0,0 +1,13 @@
+// Expression
+
+3 + 4;
+1990;
+true && false && !false
+
+// Statement
+
+if (23 > 10) {
+    const str = "23 is bigger";
+}
+
+// Don't use Statement inside template literal only use Expression if needed.
