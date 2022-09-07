3991a1e4bf705c79cc4a35c5259b779c166c4cd5
ArRolin <gmarrolin@gmail.com>
2022-09-07 18:48:36 +0600

    31 Introduction to Objects topic Added

---
+++ 31_Introduction_to_Objects/script.js
@@ -0,0 +1,16 @@
+
+const ashiqArray = [
+    "Ashiqur",
+    "Rahaman",
+    2022 - 1990,
+    "Software Engineer",
+    ["Monir", "Masum", "Mahfuz"]
+]
+
+const ashiq = {
+    firstName: "Ashiqur",
+    lastName: "Rahaman",
+    age: 2022 - 1990,
+    job: "Software Engineer",
+    friends: ["Monir", "Masum", "Mahfuz"]
+}
